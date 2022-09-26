7354ebfd805a95551daf7098aa51150868b0094a
slv-improver <k.moret.slv@gmail.com>
2022-09-26 22:02:32 +0100

    add some comments

--- multiplication.py
+++ multiplication.py
@@ -5,23 +5,26 @@ import pyautogui
 
 def exit():
     print('Temps dépassé')
+    # autopress enter key
     pyautogui.hotkey('enter')
 
+# define variables for the game
 time_limit = int(input('Combien de temps pour répondre ? '))
+table = int(input('Quel table ? '))
 errors = 0
 points = 0
 nb_of_question = 0
-table = int(input('Quel table ? '))
 
 while errors < 10:
     first_number = table or random.randrange(2, 11)
     second_number = random.randrange(2, 11)
     multiplication = first_number * second_number
-
+    # start timer
     t = Timer(time_limit, exit)
     t.start()
     answer = input(f'{first_number} x {second_number} = ...')
     answer = int(answer or 0)
+    # timer call callback (exit())
     t.cancel()
     nb_of_question += 1
     if answer == multiplication:
